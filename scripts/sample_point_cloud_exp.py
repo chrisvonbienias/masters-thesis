5ae457c623aae3d3670cfe60e0e37add77cae9c6
Krzysztof Bieniek <chrisvonbienias@gmail.com>
2022-03-16 16:10:01 +0100

    Copied function from library to study

--- scripts/sample_point_cloud_exp.py
+++ scripts/sample_point_cloud_exp.py
@@ -1,1 +1,68 @@
 # Point sampling algorithm from trimesh library
+
+import numpy as np
+
+def sample_surface(mesh, count, face_weight=None):
+    """
+    Sample the surface of a mesh, returning the specified
+    number of points
+    For individual triangle sampling uses this method:
+    http://mathworld.wolfram.com/TrianglePointPicking.html
+    Parameters
+    -----------
+    mesh : trimesh.Trimesh
+      Geometry to sample the surface of
+    count : int
+      Number of points to return
+    face_weight : None or len(mesh.faces) float
+      Weight faces by a factor other than face area.
+      If None will be the same as face_weight=mesh.area
+    Returns
+    ---------
+    samples : (count, 3) float
+      Points in space on the surface of mesh
+    face_index : (count,) int
+      Indices of faces for each sampled point
+    """
+
+    if face_weight is None:
+        # len(mesh.faces) float, array of the areas
+        # of each face of the mesh
+        face_weight = mesh.area_faces
+
+    # cumulative sum of weights (len(mesh.faces))
+    weight_cum = np.cumsum(face_weight)
+
+    # last value of cumulative sum is total summed weight/area
+    face_pick = np.random.random(count) * weight_cum[-1]
+    # get the index of the selected faces
+    face_index = np.searchsorted(weight_cum, face_pick)
+
+    # pull triangles into the form of an origin + 2 vectors
+    tri_origins = mesh.triangles[:, 0]
+    tri_vectors = mesh.triangles[:, 1:].copy()
+    tri_vectors -= np.tile(tri_origins, (1, 2)).reshape((-1, 2, 3))
+
+    # pull the vectors for the faces we are going to sample from
+    tri_origins = tri_origins[face_index]
+    tri_vectors = tri_vectors[face_index]
+
+    # randomly generate two 0-1 scalar components to multiply edge vectors by
+    random_lengths = np.random.random((len(tri_vectors), 2, 1))
+
+    # points will be distributed on a quadrilateral if we use 2 0-1 samples
+    # if the two scalar components sum less than 1.0 the point will be
+    # inside the triangle, so we find vectors longer than 1.0 and
+    # transform them to be inside the triangle
+    random_test = random_lengths.sum(axis=1).reshape(-1) > 1.0
+    random_lengths[random_test] -= 1.0
+    random_lengths = np.abs(random_lengths)
+
+    # multiply triangle edge vectors by the random lengths and sum
+    sample_vector = (tri_vectors * random_lengths).sum(axis=1)
+
+    # finally, offset by the origin to generate
+    # (n,3) points in space on the triangle
+    samples = sample_vector + tri_origins
+
+    return samples, face_index
