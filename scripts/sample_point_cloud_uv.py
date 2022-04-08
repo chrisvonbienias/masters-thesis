de054a9d6ea8e14c520bfdc71a544b8981b4b840
Krzysztof Bieniek <chrisvonbienias@gmail.com>
2022-04-08 20:46:02 +0200

    Updated

--- scripts/sample_point_cloud_uv.py
+++ scripts/sample_point_cloud_uv.py
@@ -1,10 +1,17 @@
-# Scripts for sampling a 3D mesh with UV colour information
+# Script for sampling 3D meshes in ShapeNet with UV colour information
+# Sampled meshes are exported to corresponding folders as PLY
 
+import os
 import pymeshlab as pml
 
-mesh = pml.MeshSet()
-mesh.load_new_mesh('resources/3d_models/001/model_fixed.obj')
-mesh.generate_sampling_montecarlo(samplenum=2048)
-mesh.transfer_texture_to_color_per_vertex(sourcemesh=0, targetmesh=1)
-mesh.save_current_mesh('resources/3d_models/001/model_fixed.ply')
-
+models_path = 'resources/3d_models/'
+folders = os.listdir(models_path)
+folders.pop(-1)  # removes '3d_models.txt'
+'''
+for folder in folders:
+    mesh = pml.MeshSet()
+    mesh.load_new_mesh(models_path + folder + 'model.obj')
+    mesh.generate_sampling_montecarlo(samplenum=2048)
+    mesh.transfer_texture_to_color_per_vertex(sourcemesh=0, targetmesh=1)
+    mesh.save_current_mesh(models_path + folder + 'model.ply')
+'''
