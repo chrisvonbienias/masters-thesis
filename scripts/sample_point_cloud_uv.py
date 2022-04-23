4cc3c5750f869c5d741fa04f735f79142512039b
Krzysztof Bieniek <chrisvonbienias@gmail.com>
2022-04-23 21:07:40 +0200

    Working and optimised version

--- scripts/sample_point_cloud_uv.py
+++ scripts/sample_point_cloud_uv.py
@@ -1,17 +1,40 @@
-# Script for sampling 3D meshes in ShapeNet with UV colour information
-# Sampled meshes are exported to corresponding folders as PLY
+# Script for converting an .obj 3D model into 2048 colored point cloud as a .ply file
+# Originally, it was supposed to be done "on the fly", when data set is imported,
+# but it is too computationally expensive.
 
-import os
 import pymeshlab as pml
+import os
+from tqdm import tqdm
 
-models_path = 'resources/3d_models/'
-folders = os.listdir(models_path)
-folders.pop(-1)  # removes '3d_models.txt'
-'''
-for folder in folders:
-    mesh = pml.MeshSet()
-    mesh.load_new_mesh(models_path + folder + 'model.obj')
-    mesh.generate_sampling_montecarlo(samplenum=2048)
-    mesh.transfer_texture_to_color_per_vertex(sourcemesh=0, targetmesh=1)
-    mesh.save_current_mesh(models_path + folder + 'model.ply')
-'''
+
+def main():
+    models_path = 'resources/3d_models/'
+    models = os.listdir(models_path)
+    # remove .mtl and .jpg files
+    models = [x for x in models if x[-1] == 'j']
+
+    removed_models = []
+    for model in tqdm(models, desc='Processing models...', position=0, leave=False):
+        mesh_path = models_path + model
+        mesh = pml.MeshSet()
+        mesh.load_new_mesh(mesh_path)
+
+        if mesh.current_mesh().texture_number():  # if mesh has a texture
+            mesh.generate_sampling_montecarlo(samplenum=2048)
+            mesh.transfer_texture_to_color_per_vertex(sourcemesh=0, targetmesh=1)
+            ply_path = mesh_path.split(sep='.')[0] + '.ply'
+            mesh.save_current_mesh(ply_path)
+            # print(f'Saving {ply_path}')
+        else:
+            removed_models.append(mesh)
+            os.remove(models_path + model)
+            mtl_path = model.split('.')[0]
+            os.remove(models_path + mtl_path + '.mtl')
+            # print(f'[WARNING] Model {model} has no texture')
+
+    print('Operation finished')
+    print(f'Removed {len(removed_models)} models')
+
+
+if __name__ == '__main__':
+    main()
