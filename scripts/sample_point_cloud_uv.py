e6b14e3d3e187ad010e1a826c701d7d16bb5a241
Krzysztof Bieniek <chrisvonbienias@gmail.com>
2022-04-25 13:58:03 +0200

    Added function for deleting models belonging to unused categories

--- scripts/sample_point_cloud_uv.py
+++ scripts/sample_point_cloud_uv.py
@@ -1,13 +1,41 @@
 # Script for converting an .obj 3D model into 2048 colored point cloud as a .ply file
 # Originally, it was supposed to be done "on the fly", when data set is imported,
 # but it is too computationally expensive.
+# Combine delete_undesired() with main() {models can be checked before converting them to PLY}
 
 import pymeshlab as pml
 import os
 from tqdm import tqdm
+import pandas as pd
 
 
-def main():
+# Script for deleting models belonging to undesirable classes
+def delete_undesired():
+    models_dir = './resources/3d_models/'
+    csv_file = pd.read_csv('resources/metadata.csv')
+    csv_file = pd.DataFrame(csv_file, columns=['fullId', 'category', 'wnsynset', 'wnlemmas'])
+    del_cat = ['room', 'court', 'courtyard', 'person', 'homo,man,human being,human', ]
+
+    deleted = 0
+    for item in tqdm(del_cat, desc='Processing models...'):
+        del_data = csv_file.loc[csv_file['wnlemmas'] == item]
+
+        for data in del_data['fullId']:
+            data = data.split(sep='.')[1]
+            path = os.path.join(models_dir, data)
+            try:
+                os.remove(path + '.obj')
+                os.remove(path + '.mtl')
+                os.remove(path + '.ply')
+                deleted += 1
+            except FileNotFoundError:
+                print(f'[WARNING] File {data} doesn\'t exist')
+
+    print("Operation finished")
+    print(f"Deleted {deleted} models")
+
+
+def main() -> None:
     models_path = 'resources/3d_models/'
     models = os.listdir(models_path)
     # remove .mtl and .jpg files
@@ -32,8 +60,10 @@ def main():
             os.remove(models_path + mtl_path + '.mtl')
             # print(f'[WARNING] Model {model} has no texture')
 
-    print('Operation finished')
+    print('Models converted')
     print(f'Removed {len(removed_models)} models')
+
+    delete_undesired()
 
 
 if __name__ == '__main__':
