fcc70ccd43ec477519bd6c3f9c6dbe288b67d28a
chrisvonbienias <chrisvonbienias@gmail.com>
2022-07-27 10:26:57 +0200

    cachanged chamfer distance to 6d

--- utils/metrics/CD/__init__.py
+++ utils/metrics/CD/__init__.py
@@ -1,4 +1,5 @@
-from .chamfer3D.dist_chamfer_3D import chamfer_3DDist as cd
+#from .chamfer3D.dist_chamfer_3D import chamfer_3DDist as cd
+from .chamfer6D.dist_chamfer_6D import chamfer_6DDist as cd
 from .fscore import fscore
 
 __all__ = ['cd', 'fscore']
