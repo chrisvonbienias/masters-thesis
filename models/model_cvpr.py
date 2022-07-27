e44bb2e60bdada764cbee2051b6e577848c085f3
chrisvonbienias <chrisvonbienias@gmail.com>
2022-07-27 10:27:42 +0200

    CVPR

---
+++ models/model_cvpr.py
@@ -0,0 +1,242 @@
+from math import log2, sqrt
+from typing import List
+
+import torch
+import torch.nn as nn
+
+from torchsummary import summary
+from pytorch3d.ops import sample_farthest_points, ball_query, knn_gather
+
+
+class CVPR_Generator(nn.Module):
+    def __init__(self, num_dense=8192, num_coarse=1024, latent_dim=1024):
+        super().__init__()
+        
+        self.num_dense = num_dense
+        self.num_coarse = num_coarse
+        self.latent_dim = latent_dim
+        self.feature_size = torch.load('checkpoint/PointAutoEncoder/mean_features.pt')
+        self.ratio = num_dense / num_coarse / 2
+        
+        self.mlp_conv128_256 = nn.Sequential(
+            nn.Conv1d(6, 128, 1, bias=False),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(128, 256, 1)
+        )
+        
+        self.mlp_conv512_lat = nn.Sequential(
+            nn.Conv1d(512, 512, 1, bias=False),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(512, self.latent_dim, 1)
+        )
+            
+        self.mlp = nn.Sequential(
+            nn.Linear(self.latent_dim, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, self.num_coarse * 6),
+        )
+        
+        self.mlp_conv128_64 = nn.Sequential(
+            nn.Conv1d(1160, 128, 1, bias=False),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(128, 64, 1)
+        )
+        
+        self.final_conv = nn.Sequential(
+            nn.Conv1d(64, 512, 1, bias=False),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(512, 512, 1, bias=False),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(512, 6, 1)
+        )
+        
+        self.features = nn.Sequential(
+            nn.Linear(1024, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True)
+        )
+        
+        self.contract1 = nn.Sequential(
+            nn.Conv2d(64, 64, (1, 1), padding='valid'),
+            nn.ReLU(inplace=True)
+        )
+        
+        self.contract2 = nn.Sequential(
+            nn.Conv2d(128, 64, (1, 1), padding='valid'),
+            nn.ReLU(inplace=True)
+        )
+        
+        self.expand = nn.Sequential(
+            nn.Conv2d(64, 128, (1, 1), padding='valid'),
+            nn.ReLU(inplace=True)
+        )
+        
+    
+    def generate_grid(self, ratio):
+        sqrted = int(sqrt(ratio)) + 1
+        for i in range(1, sqrted + 1).__reversed__():
+            if (ratio % i) == 0:
+                num_x = i
+                num_y = ratio // 1
+                break
+        grid_x = torch.linspace(-0.2, 0.2, num_x)
+        grid_y = torch.linspace(-0.2, 0.2, num_y)
+        x, y = torch.meshgrid(grid_x, grid_y, indexing='ij')
+        grid = torch.reshape(torch.stack([x, y], dim=-1), (-1, 2))
+        return grid.cuda()
+    
+    
+    def forward(self, xyz):
+        B, N, _ = xyz.shape
+        
+        # encoder
+        features = self.mlp_conv128_256(xyz.transpose(2, 1))                         # (B, 256, N)
+        features_global = torch.max(features, dim=2, keepdim=True)[0]                # (B, 256, 1)
+        features = torch.cat([features, features_global.expand(-1, -1, N)], dim=1)   # (B, 512, N)
+        features = self.mlp_conv512_lat(features)                                    # (B, latent_dim, N)
+        features = torch.max(features, dim=2)[0]                                     # (B, latent_dim)
+        
+        # decoder
+        
+        # coarse
+        coarse = self.mlp(features)                                                  # (B, num_coarse * 6)
+        coarse = torch.tanh(coarse)
+        coarse = torch.reshape(coarse, (-1, self.num_coarse, 6))                     # (B, num_coarse, 6)
+        
+        # mirror + fps
+        input_fps = sample_farthest_points(xyz, K=self.num_coarse // 2)[0]           # (B, num_coarse / 2, 6)
+        input_fps_filp = torch.cat(
+            [torch.unsqueeze(input_fps[:, :, 0], dim=2),
+             torch.unsqueeze(input_fps[:, :, 1], dim=2),
+             torch.unsqueeze(-input_fps[:, :, 2], dim=2),
+             torch.unsqueeze(input_fps[:, :, 3], dim=2),
+             torch.unsqueeze(input_fps[:, :, 4], dim=2),
+             torch.unsqueeze(input_fps[:, :, 5], dim=2),], dim=2
+        )                                                                            # (B, num_coarse / 2, 6)
+        input_fps = torch.cat([input_fps, input_fps_filp], dim=1)                    # (B, num_coarse, 6)
+        coarse2 = torch.cat([input_fps, coarse], dim=1)                              # (B, 2 * num_coarse, 6)                     
+        
+        # lifting module
+        for i in range(int(log2(self.ratio))):
+            num_fine = 2 ** (i + 2) * self.num_coarse
+            grid = self.generate_grid(2 ** (i + 1))
+            grid = torch.unsqueeze(grid, dim=0)
+            grid_feat = torch.tile(grid, (B, self.num_coarse, 1))
+            point_feat = torch.tile(torch.unsqueeze(coarse2, 2), (1, 1, 2, 1))
+            point_feat = torch.reshape(point_feat, (-1, num_fine, 6))
+            global_feat = torch.tile(torch.unsqueeze(features, 1), (1, num_fine, 1))
+            
+            mean_feature = self.features(self.feature_size.expand((B, -1)))
+            mean_feature = torch.unsqueeze(mean_feature, dim=1)
+            mean_feature = torch.tile(mean_feature, [1, num_fine, 1])
+            feat = torch.cat([grid_feat, point_feat, global_feat, mean_feature], dim=2)
+            
+            # contract-expand operation
+            feat1 = self.mlp_conv128_64(feat.transpose(2, 1))
+            feat2 = torch.reshape(feat1, (feat1.shape[0], 1, -1, feat1.shape[-1]))
+            feat2 = torch.transpose(feat2, 2, 1)
+            feat2 = self.contract1(feat2)
+            feat2 = self.expand(feat2)
+            feat2 = torch.reshape(feat2, (feat2.shape[0], 128, 1, -1))
+            feat2 = self.contract2(feat2)
+            feat2 = torch.squeeze(feat2, dim=2)
+            feat = feat1 + feat2
+            
+            fine = self.final_conv(feat)
+            coarse2 = fine
+            
+        return coarse.contiguous(), fine.transpose(2, 1).contiguous()
+ 
+
+class CVPR_Discriminator(nn.Module):
+    def __init__(self, radius_list: List[int]=[0.1, 0.2, 0.4], nsample_list: List[int]=[16, 32, 128]):
+        super().__init__()
+        assert len(nsample_list) == len(radius_list) 
+        
+        self.radius_list = radius_list
+        self.nsample_list = nsample_list
+        
+        self.mplConv_2 = nn.Sequential(
+            nn.Conv1d(64, 64, 1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv1d(64, 64, 1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv1d(64, 128, 1, bias=False),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(inplace=True),
+        )
+        
+        self.mplConv_3 = nn.Sequential(
+            nn.Conv1d(128, 64, 1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv1d(64, 96, 1, bias=False),
+            nn.BatchNorm1d(96),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv1d(96, 128 * 6, 1, bias=False),
+            nn.BatchNorm1d(128 * 6),
+            nn.LeakyReLU(inplace=True),
+        )
+        
+        self.mlpConv_4 = nn.Sequential(
+            nn.Conv1d(18, 1, 1, bias=False),
+            nn.BatchNorm1d(1),
+            nn.ReLU(inplace=True)
+        )
+    
+    
+    def conv1d(self, input):
+            
+        Conv1d = nn.Sequential(
+            nn.Conv1d(input.shape[1], 32, 1, bias=False),
+            nn.BatchNorm1d(32),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv1d(32, 32, 1, bias=False),
+            nn.BatchNorm1d(32),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv1d(32, 64, 1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(inplace=True),
+        ).cuda()
+        
+        output = Conv1d(input)
+        
+        return output.cuda()
+        
+        
+    def forward(self, xyz):
+        B, N, _ = xyz.shape
+        
+        points, _ = sample_farthest_points(xyz, K= N // 8)
+        
+        points_list = []
+        for (radius, nsample) in zip(self.radius_list, self.nsample_list):
+            new_xyz, _, _ = ball_query(points, xyz, None, None, nsample, radius)
+            new_xyz = self.conv1d(new_xyz.transpose(2, 1))
+            new_xyz = self.mplConv_2(new_xyz)
+            new_xyz = self.mplConv_3(new_xyz)
+            new_xyz = torch.reshape(new_xyz, (B, N // 8, 6, -1))
+            new_xyz = torch.max(new_xyz, dim=-1)[0]
+            points_list.append(new_xyz)
+        
+        output = torch.cat(points_list, dim=-1)
+        patch_values = self.mlpConv_4(output.transpose(2,1))
+        patch_values = torch.mean(patch_values, dim=-1)
+        
+        return patch_values.contiguous()
+        
+        
+        
+if __name__ == '__main__':
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = CVPR_Discriminator().to(device)
+    summary(model, (8192, 6))
---
+++ train_gan.py
@@ -0,0 +1,198 @@
+from tqdm import tqdm
+from time import time, localtime
+
+import torch
+import torch.optim as Optim
+from torch.utils.data.dataloader import DataLoader
+
+from models import CVPR_Generator, CVPR_Discriminator
+from dataset import ShapeNet
+from logger import Logger
+from utils.model_utils import  calc_cd, calc_dcd
+
+# hyperparameters
+DEBUG = False
+EPOCHS = 50
+BATCH_SIZE = 16
+LEARNING_RATE = 0.001
+LOSS = 'CD'
+NUM_DENSE = 8192
+NUM_WORKERS = 8
+ALPHA = 0.5
+LOAD = False
+
+
+# disable debug API for final training
+# use cuDNN Autotuner
+if (not DEBUG):
+    torch.autograd.set_detect_anomaly(False)
+    torch.autograd.profiler.profile(False)
+    torch.autograd.profiler.emit_nvtx(False)
+    torch.backends.cudnn.enabled = True
+    torch.backends.cudnn.benchmark = True
+else:
+    torch.autograd.set_detect_anomaly(True)
+    torch.autograd.profiler.profile(True)
+    torch.autograd.profiler.emit_nvtx(True)
+    
+    
+def train() -> None:
+    
+    data_path = "./resources/dataset"
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f'Device: {device}')
+    
+    # time stamp
+    time_stamp = localtime(time())
+    save_path_gen = f'checkpoint/CVPR/CVPR_GEN_{LOSS}_{time_stamp.tm_mon}_{time_stamp.tm_mday}_{time_stamp.tm_hour}:{time_stamp.tm_min}'
+    save_path_disc = f'checkpoint/CVPR/CVPR_DICS_{LOSS}_{time_stamp.tm_mon}_{time_stamp.tm_mday}_{time_stamp.tm_hour}:{time_stamp.tm_min}'
+
+    # datasets
+    train_dataset = ShapeNet(data_path, "train", num_dense=NUM_DENSE)
+    validate_dataset = ShapeNet(data_path, "validate", num_dense=NUM_DENSE)
+    print('Datasets loaded!')
+
+    # dataloaders
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS, pin_memory=True)
+    validate_loader = DataLoader(validate_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS, pin_memory=True)
+    print('Dataloaders ready!')
+
+    # model
+    model_gen = CVPR_Generator(num_dense=NUM_DENSE).to(device)
+    model_disc = CVPR_Discriminator().to(device)
+    
+    # load model.state_dict
+    if LOAD:
+        model_gen.load_state_dict(torch.load(''))
+        model_disc.load_state_dict(torch.load(''))
+    
+    # optimizer
+    # optimizer = Optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    optimizer_gen = Optim.AdamW(model_gen.parameters(), lr=LEARNING_RATE)
+    optimizer_disc = Optim.AdamW(model_disc.parameters(), lr=LEARNING_RATE)
+    # lr_schedual = Optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+    lr_schedual_gen = Optim.lr_scheduler.ReduceLROnPlateau(optimizer_gen, 'min', patience = 5)
+    lr_schedual_disc = Optim.lr_scheduler.ReduceLROnPlateau(optimizer_disc, 'min', patience = 5)
+    
+    # logger
+    logger_gen = Logger(EPOCHS, model_name='CVPR_Generator')
+    logger_disc = Logger(EPOCHS, model_name='CVPR_Discriminator')
+    print('Logger ready!')
+
+    # training
+    best_val_gen = 100
+    best_val_disc = 100
+    for epoch in tqdm(range(1, EPOCHS + 1), desc='Epoch', position=0):
+        
+        # training
+        train_loss_gen = 0.0
+        train_loss_disc = 0.0
+        i = 0
+        model_gen.train()
+        model_disc.train()
+        for c, p in tqdm(train_loader, desc='Training', position=1, leave=False):
+            c, p = c.to(device), p.to(device)
+            
+            ### GENERATOR ###
+                        
+            # forward propagation
+            coarse_pred, dense_pred = model_gen(p)
+            coarse_pred = coarse_pred.to(device)
+            dense_pred = dense_pred.to(device)
+
+            # loss function
+            loss1 = torch.mean(calc_cd(dense_pred, c)[0]).to(device)
+            loss2 = torch.mean(calc_cd(coarse_pred, c)[0]).to(device)
+            loss_gen = loss1 + ALPHA * loss2
+
+            
+            ### DISCRIMINATOR
+            
+            # forward propagation
+            d_fake = model_disc(dense_pred)
+            d_fake = d_fake.to(device)
+            d_real = model_disc(c.detach())
+            d_real = d_real.to(device)
+            
+            # loss function
+            loss_fake = torch.mean(d_fake ** 2)
+            loss_real = torch.mean((d_real - 1) ** 2)
+            loss_disc1 = 0.5 * (loss_fake + loss_real)
+            loss_disc2 = torch.mean((loss_fake - 1) ** 2)
+            
+            ### TOTAL LOSS
+            loss_gen  = loss_disc1 + loss_gen * 200
+            loss_disc = loss_disc2
+            
+            ### BACK PROPAGATION
+            optimizer_gen.zero_grad(set_to_none=True)
+            loss_gen.backward(retain_graph=True)
+            
+            optimizer_disc.zero_grad(set_to_none=True)
+            loss_disc.backward()
+            
+            optimizer_gen.step()
+            optimizer_disc.step()
+            
+            train_loss_gen += loss_gen.item()
+            train_loss_disc += loss_disc.item()
+            
+            i += 1
+
+        logger_gen.train_data.append(float(train_loss_gen / i))
+        logger_disc.train_data.append(float(train_loss_disc / i))
+
+        # evaluation
+        model_gen.eval()
+        model_disc.eval()
+        total_loss_gen = 0.0
+        total_loss_disc = 0.0
+
+        with torch.no_grad():
+            i = 0
+            for c, p in tqdm(validate_loader, desc='Validation', position=1, leave=False):
+                c, p = c.to(device), p.to(device)
+                
+                # generator
+                coarse_pred, dense_pred = model_gen(p)
+                dense_pred = dense_pred.to(device)
+                
+                # discriminator
+                d_fake = model_disc(dense_pred)
+                d_fake = d_fake.to(device)
+            
+                total_loss_gen += torch.mean(calc_cd(dense_pred, c)[0])
+                total_loss_disc += torch.mean((loss_fake - 1) ** 2)
+                i += 1
+        
+        total_loss_gen /= i
+        total_loss_disc /= i
+        logger_gen.val_data.append(float(total_loss_gen))
+        logger_disc.val_data.append(float(total_loss_disc))
+        lr_schedual_gen.step(float(total_loss_gen))
+        lr_schedual_disc.step(float(total_loss_disc))
+        
+        if ((total_loss_gen / i) < best_val_gen):
+            best_val_gen = total_loss_gen / i
+            torch.save(model_gen.state_dict(), f'{save_path_gen}.pth')
+            logger_gen.best_data.append(1)
+        else:
+            logger_gen.best_data.append(0)
+            
+        if ((total_loss_disc / i) < best_val_disc):
+            best_val_disc = total_loss_disc / i
+            torch.save(model_disc.state_dict(), f'{save_path_disc}.pth')
+            logger_disc.best_data.append(1)
+        else:
+            logger_disc.best_data.append(0)
+            
+        logger_gen.save(save_path_gen)
+        logger_disc.save(save_path_disc)
+            
+        # show values
+        # print(f'Train loss: {logger.train_data[-1]}')
+        # print(f'Val loss: {logger.val_data[-1]}')
+
+    
+if __name__ == "__main__":
+    train()
