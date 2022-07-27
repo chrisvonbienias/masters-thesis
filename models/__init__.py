40fefe3f5a37e5a56f2239e66571b178389fb192
chrisvonbienias <chrisvonbienias@gmail.com>
2022-07-27 10:28:44 +0200

    new models

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,6 @@
+import imp
 from models.model_pcn import PCN
+from models.model_finerpcn import FinerPCN
+from models.model_autoencoder import PointAutoEncoder
+from models.model_cvpr import CVPR_Generator, CVPR_Discriminator
+from models.model_msn import MSN
