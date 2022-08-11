706c536da151fd9e18a685b6392baf6445cc1d97
chrisvonbienias <chrisvonbienias@gmail.com>
2022-08-11 21:06:11 +0200

    implemented decoder and minium density sampling

--- models/CPN/model_cpn.py
+++ models/CPN/model_cpn.py
@@ -1,29 +1,59 @@
+import sys
+
 import torch
 import torch.nn as nn
 
 from pvconv import PVConv
 from shared_mlp import SharedMLP
 
+sys.path.append("/home/kbieniek/Desktop/masters-thesis/utils/expansion_penalty")
+import expansion_penalty_module as expansion
+sys.path.append("/home/kbieniek/Desktop/masters-thesis/utils/MDS")
+import MDS_module
+
+class MorphingDecoder(nn.Module):
+    def __init__(self, latent_dim=1024) -> None:
+        super().__init__()
+        
+        self.latent_dim = latent_dim
+        self.decoder = nn.Sequential(
+            nn.Conv1d(self.latent_dim, self.latent_dim, 1, bias=False),
+            nn.ELU(),
+            nn.Conv1d(self.latent_dim, self.latent_dim // 2, 1, bias=False),
+            nn.ELU(),
+            nn.Conv1d(self.latent_dim // 2, self.latent_dim // 4, 1, bias=False),
+            nn.ELU(),
+            nn.Conv1d(self.latent_dim // 4, 6, 1, bias=False),
+            nn.ELU(),
+        )
+        
+        
+    def forward(self, input):
+        return self.decoder(input)
+
 
 class CPN(nn.Module):
-    def __init__(self, num_dense=8192, latent_dim=1024) -> None:
+    def __init__(self, num_dense=8192, latent_dim=1024, num_primitives=8) -> None:
         super().__init__()
         
         self.num_dense = num_dense
         self.num_coarse = num_dense // 4
         self.latent_dim = latent_dim
+        self.num_primitives = num_primitives
         
-        self.pv_conv = nn.Sequential(
-            PVConv(6, 2048, kernel_size=3, resolution=16),
-            PVConv(2048, 1024, kernel_size=3, resolution=16),
-            PVConv(1024, 512, kernel_size=3, resolution=8)
+        self.encoder = nn.Sequential(
+            PVConv(6, 64, kernel_size=3, resolution=16),
+            PVConv(64, 128, kernel_size=3, resolution=16),
+            PVConv(128, 512, kernel_size=3, resolution=8),
+            PVConv(512, self.latent_dim, kernel_size=3, resolution=4)
         )
         
-        self.shared_mlp = nn.Sequential(
-            SharedMLP(6, 2048),
-            SharedMLP(2048, 1024),
-            SharedMLP(1024, 512),
+        self.linear = nn.Sequential(
+            nn.Linear(self.latent_dim, self.latent_dim),
+            nn.ELU()
         )
+        
+        self.decoder = nn.ModuleList(MorphingDecoder(self.latent_dim + 2) for i in range(0, self.num_primitives))
         
         
     def forward(self, xyz):
@@ -31,14 +61,34 @@ class CPN(nn.Module):
         
         # ENCODER
         xyz = xyz.transpose(2, 1)
-        features1, _ = self.pv_conv((xyz, xyz[:, :3, :]))
-        features2 = self.shared_mlp(xyz)
-        features = torch.cat((features1, features2), dim=1)
+        features, _ = self.encoder((xyz, xyz[:, :3, :]))
         features = torch.max(features, dim=2)[0]
+        features = self.linear(features)
         
         # DECODER
+        outs = []
+        for i in range(0, self.num_primitives):
+            rand_grid = torch.randn((B, 2, self.num_coarse // self.num_primitives), dtype=torch.float32, device='cuda:0')
+            y = features.unsqueeze(2).expand(features.size(0),features.size(1), rand_grid.size(2))
+            y = torch.cat((rand_grid, y), dim=1)
+            outs.append(self.decoder[i](y))
+            
+        outs = torch.cat(outs, 2)
+        coarse = outs.transpose(2, 1) # (B, DENSE, 6)
         
-        return features
+        # EXPANSION LOSS
+        dist, _, mean_mst_dis = self.expansion(coarse, self.num_coarse // self.num_primitives, 1.5)
+        exp_loss = torch.mean(dist)
+        
+        # MINIMUM DENSITY SAMPLE
+        dense = torch.cat((xyz.tranpose(2, 1), coarse), dim=1)
+        mds_idx = MDS_module.minimum_density_sample(dense.contiguous(), dense.shape(1), mean_mst_dis)
+        dense = MDS_module.gather_operation(dense, mds_idx)
+        
+        # RESIDUAL NETWORK
+        
+        
+        return coarse, dense, exp_loss
     
     
 if __name__ == '__main__':
