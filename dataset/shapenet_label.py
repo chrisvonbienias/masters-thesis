8f66d9399872220a409c0be8c255ee51d61318ad
chrisvonbienias <chrisvonbienias@gmail.com>
2022-08-17 01:34:52 +0200

    shapenetlabel dataset

--- dataset/shapenet_label.py
+++ dataset/shapenet_label.py
@@ -8,59 +8,59 @@ import plyfile
 import json
 from skimage.color import rgb2lab
 
-class ShapeNet(data.Dataset):
+class ShapeNetLabel(data.Dataset):
     
     def __init__(self, data_path, split, num_dense=8192, subset=None) -> None:
         assert split in ["train", "test_seen", "test_unseen", "validate"], "split error value!"
         
         self.seen_cat = [
-            "02691156" # airplane,aeroplane,plane
-            "02747177" # ashcan,trash can,garbage can,wastebin,ash bin,ash-bin,ashbin,dustbin,trash barrel,trash bin
-            "02773838" # bag,traveling bag,travelling bag,grip,suitcase
-            "02801938" # basket,handbasket
-            "02808440" # bathtub,bathing tub,bath,tub
-            "02843684" # birdhouse
-            "02876657" # bottle
-            "02880940" # bowl
-            "02933112" # cabinet
-            "02942699" # camera,photographic camera
-            "02946921" # can,tin,tin can
-            "02954340" # cap
-            "02958343" # car,auto,automobile,machine,motorcar
-            "03001627" # chair
-            "03046257" # clock
-            "03085013" # computer keyboard,keypad
-            "03207941" # dishwasher,dish washer,dishwashing machine
-            "03211117" # display,video display
-            "03261776" # earphone,earpiece,headphone,phone
-            "03325088" # faucet,spigot
-            "03337140" # file,file cabinet,filing cabinet
-            "03513137" # helmet
-            "03593526" # jar
-            "03624134" # knife
-            "03636649" # lamp
-            "03642806" # laptop,laptop computer
-            "03691459" # loudspeaker,speaker,speaker unit,loudspeaker system,speaker system
-            "03710193" # mailbox,letter box
-            "03759954" # microphone,mike
-            "03761084" # microwave,microwave oven
-            "03797390" # mug
-            "03928116" # piano,pianoforte,forte-piano
-            "03938244" # pillow
-            "03991062" # pot,flowerpot
-            "04004475" # printer,printing machine
-            "04074963" # remote control,remote
-            "04090263" # rifle
-            "04099429" # rocket,projectile
-            "04256520" # sofa,couch,lounge
-            "04330267" # stove
-            "04379243" # table
-            "04401088" # telephone,phone,telephone set
-            "02992529" # cellular telephone,cellular phone,cellphone,cell,mobile phone
-            "04460130" # tower
-            "04468005" # train,railroad train
-            "04530566" # vessel,watercraft
-            "04554684" # washer,automatic washer,washing machine
+            "02691156", # airplane
+            "02747177", # ashcan
+            "02773838", # bag
+            "02801938", # basket
+            "02808440", # bathtub
+            "02843684", # birdhouse
+            "02876657", # bottle
+            "02880940", # bowl
+            "02933112", # cabinet
+            "02942699", # camera
+            "02946921", # can
+            "02954340", # cap
+            "02958343", # car
+            "03001627", # chair
+            "03046257", # clock
+            "03085013", # computer keyboard
+            "03207941", # dishwasher
+            "03211117", # display
+            "03261776", # earphone
+            "03325088", # faucet
+            "03337140", # file
+            "03513137", # helmet
+            "03593526", # jar
+            "03624134", # knife
+            "03636649", # lamp
+            "03642806", # laptop
+            "03691459", # loudspeaker
+            "03710193", # mailbox
+            "03759954", # microphone
+            "03761084", # microwave
+            "03797390", # mug
+            "03928116", # piano
+            "03938244", # pillow
+            "03991062", # pot
+            "04004475", # printer
+            "04074963", # remote control
+            "04090263", # rifle
+            "04099429", # rocket
+            "04256520", # sofa
+            "04330267", # stove
+            "04379243", # table
+            "04401088", # telephone
+            "02992529", # cellular telephone
+            "04460130", # tower
+            "04468005", # train
+            "04530566", # watercraft
+            "04554684", # washing machine
         ]
 
         self.unseen_cat = [
@@ -89,13 +89,16 @@ class ShapeNet(data.Dataset):
         if torch.is_tensor(index):
             index = index.tolist()
 
-        path = self.paths[index]
+        path, label = self.paths[index]
         complete_path = f'{self.data_path}/complete/{path}.ply'
         partial_path = f'{self.data_path}/partial/{path}.ply'
         complete = self.random_sample(self.read_point_cloud(complete_path), self.num_dense)
         partial = self.random_sample(self.read_point_cloud(partial_path), self.num_dense // 4)
+        
+        idx = torch.tensor(self.seen_cat.index(label) + 1)
+        hot_vector = torch.nn.functional.one_hot(idx, num_classes=len(self.seen_cat) + 1)
 
-        return torch.from_numpy(complete), torch.from_numpy(partial)
+        return torch.from_numpy(complete), torch.from_numpy(partial), hot_vector
     
         
     def load_data(self) -> List[str]:
@@ -112,7 +115,7 @@ class ShapeNet(data.Dataset):
         if self.split == "test_unseen" or self.split == "test_seen":
             self.split = "test"
 
-        for cat in categories.values():
+        for label, cat in categories.items():
             for model in cat:
                 if not os.path.exists(f'resources/dataset/complete/{model[0]}.ply'):
                     continue
@@ -120,7 +123,7 @@ class ShapeNet(data.Dataset):
                     continue
 
                 if model[1] == self.split:
-                    paths.append(model[0])
+                    paths.append((model[0], label))
                     
         if self.subset is not None:
             paths = self.random_sample(np.asarray(paths), len(paths) // self.subset)
@@ -147,3 +150,8 @@ class ShapeNet(data.Dataset):
         if idx.shape[0] < n:
             idx = np.concatenate([idx, np.random.randint(pc.shape[0], size=n-pc.shape[0])])
         return pc[idx[:n]]
+
+    
+if __name__ == '__main__':
+    train_dataset = ShapeNetLabel("/home/kbieniek/Desktop/masters-thesis/resources/dataset", "train", num_dense=4096, subset=None)
+    train_dataset.__getitem__(1)
